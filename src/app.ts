da49c80e7f4ba4dec9cf64d88dd80ecb2af81173
Cristiane Kizelevicius <cristiane@samaan.com.br>
2023-03-22 15:23:02 -0300

    cria rota Cars e Middware error

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
 import express from 'express';
+import errorMiddleware from './Middelwares/errorMiddealwares';
+import carsRoutes from './Routes/CarsRoutes';
 
 const app = express();
+
+app.use(express.json());
+app.use('/cars', carsRoutes);
+
+app.use(errorMiddleware);
 
 export default app;
 // iniciando projeto
